3f967cfa25c30fd98668599501840e588b1b2844
JohnTrump <wujunchuan1994@hotmail.com>
2017-10-09 00:22:37 +0800

    接入styleint

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "extends": "stylelint-config-standard",
+  "rules": {
+    // 统一使用 单个冒号
+    // ref stackoverflow https://stackoverflow.com/questions/10181729/should-i-use-single-or-double-colon-notation-for-pseudo-elements
+    "selector-pseudo-element-colon-notation": "single"
+  }
+}
